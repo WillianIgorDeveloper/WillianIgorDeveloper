d2362a6e455f07d950b21f01263292ed9a26a3c0
Willian Igor <willianigordeveloper@gmail.com>
2022-10-20 00:25:48 -0300

    create home section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Menu from '../components/menu'
 
+import style from '../styles/index.module.scss'
+
 export default function Home() {
   return (
     <>
@@ -9,6 +11,17 @@ export default function Home() {
         <title>Portifólio | Willian Igor</title>
       </Head>
       <Menu />
+      <main>
+        <section id='Home' className={style.home}>
+          <img 
+            src="/main-illustration.svg" 
+            alt="Ilustração de desenvolvedor" 
+            className={style.mainIllustration}
+          />
+          <h1>Willian Igor Santos</h1>
+          <strong>Devenvolvedor Front-End</strong>
+        </section>
+      </main>
     </>
   )
 }
