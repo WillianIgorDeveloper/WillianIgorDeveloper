3b227faab6ed094840ff8beb53a18cb7d02070a6
Willian Igor <willianigordeveloper@gmail.com>
2022-12-13 23:10:35 -0300

    new Website

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -40,7 +40,7 @@ export default function Projects() {
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5 lg:gap-8 text-sm md:text-base">
             <div className="card p-3 rounded-lg bg-gradient-to-b to-gray-300 from-gray-200 dark:to-gray-800 dark:from-gray-700 flex flex-col gap-2 lg:gap-3 hover:scale-105  transition-all duration-200 hover:dark:to-brand-800 hover:dark:from-brand-700 hover:to-brand-200 hover:from-brand-300">
               <Image src="/projects/ignite-lab.png" width="250" height="250" className="rounded w-full" />
-              <p className="line-clamp-2 ">Projeto desenvolvido juntamente com o "Ignite Lab | 03" da Rocketseat</p>
+              <p className="line-clamp-2 ">Projeto desenvolvido juntamente com o Ignite Lab | 03 da Rocketseat</p>
               <div className="flex gap-3 font-medium">
                 <a href="https://ignite-lab-03-green.vercel.app/" target="_blank" rel="noreferrer"><button className="py-1 bg-brand-600 hover:bg-brand-500 px-3 rounded-lg transition-colors duration-100 text-gray-100">Demo</button></a>
                 <a href="https://github.com/WillianIgorDeveloper/ignite-lab-03" target="_blank" rel="noreferrer"><button className="py-1 px-3 border rounded-lg hover:text-gray-100 hover:bg-gray-800 hover:dark:bg-gray-50 hover:dark:text-gray-800 transition-colors duration-100 border-gray-800 dark:border-gray-100">Git</button></a>
@@ -48,7 +48,7 @@ export default function Projects() {
             </div>
             <div className="card p-3 rounded-lg bg-gradient-to-b to-gray-300 from-gray-200 dark:to-gray-800 dark:from-gray-700 flex flex-col gap-2 lg:gap-3 hover:scale-105  transition-all duration-200 hover:dark:to-brand-800 hover:dark:from-brand-700 hover:to-brand-200 hover:from-brand-300">
               <Image src="/projects/to-do.png" width="250" height="250" className="rounded w-full" />
-              <p className="line-clamp-2 ">Um simples aplicativo To-Do, feito apenas com JavaScript 'Vanilla'</p>
+              <p className="line-clamp-2 ">Um simples aplicativo To-Do, feito apenas com JavaScript Vanilla</p>
               <div className="flex gap-3 font-medium">
                 <a href="https://willianigordeveloper.github.io/to-do-list/" target="_blank" rel="noreferrer"><button className="py-1 bg-brand-600 hover:bg-brand-500 px-3 rounded-lg transition-colors duration-100 text-gray-100">Demo</button></a>
                 <a href="https://github.com/WillianIgorDeveloper/to-do-list" target="_blank" rel="noreferrer"><button className="py-1 px-3 border rounded-lg hover:text-gray-100 hover:bg-gray-800 hover:dark:bg-gray-50 hover:dark:text-gray-800 transition-colors duration-100 border-gray-800 dark:border-gray-100">Git</button></a>
